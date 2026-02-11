03f780dd8e0c0cb6fa87c1162e7ce48e6bd6e085
Davenir Ramos <davenirramos@gmail.com>
2026-02-11 12:55:46 -0300

    estrutura pasta, Desenvolvimento, lógica e funcionalidade.

--- src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
@@ -1,5 +1,9 @@
 package br.com.alura.LiterAlura;
 
+import br.com.alura.LiterAlura.Principal.Principal;
+import br.com.alura.LiterAlura.Repository.AutorRepository;
+import br.com.alura.LiterAlura.Repository.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,12 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiterAluraApplication implements CommandLineRunner {
 
+    @Autowired
+    private LivroRepository repositorioLivro;
+
+    @Autowired
+    private AutorRepository repositorioAutor;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("hello!! Iniciando Desafio LiterAlura");
+        Principal menu = new Principal(repositorioLivro, repositorioAutor);
+        menu.exibeMenu();
     }
 }
---
+++ src/main/java/br/com/alura/LiterAlura/Model/Livro.java
@@ -0,0 +1,80 @@
+package br.com.alura.LiterAlura.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "livros")
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String titulo;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "autor_id")
+    private Autor autor;
+
+    private String idiomas;
+
+    public Livro(){}
+
+    public Livro(DadosLivro dadosLivro) {
+        this.titulo = dadosLivro.titulo();
+
+        if (dadosLivro.autores() != null && !dadosLivro.autores().isEmpty()) {
+              this.autor = new Autor(dadosLivro.autores().get(0));
+        }else {
+            this.autor  = null;
+        }
+
+        if ( dadosLivro.idiomas() != null && ! dadosLivro.idiomas().isEmpty()){
+            this.idiomas = String.join(",", dadosLivro.idiomas());
+        }
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getIdiomas() {
+        return idiomas;
+    }
+
+    public void setIdiomas(String idiomas) {
+        this.idiomas = idiomas;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+
+    @Override
+    public String toString() {
+        return "titulo: '" + titulo + '\'' +
+                "\nAutor: " +(autor != null ? autor.getNome() : "Desconhecido") +
+                "\nidiomas: " + idiomas + "\n";
+    }
+
+}
